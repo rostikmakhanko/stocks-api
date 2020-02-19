be8b5987d35becdc4c803da2287fc3cfd309a7c2
Rostyslav Makhanko <rostyslavm@wix.com>
2020-02-19 19:37:26 +0200

    Add search by name form opens on a search button click

--- react/src/components/App/App.tsx
+++ react/src/components/App/App.tsx
@@ -7,6 +7,7 @@ import { ReactComponent as FilterSvg } from "../../assets/filter.svg";
 import { ReactComponent as RefreshSvg } from "../../assets/refresh.svg";
 import { ReactComponent as SearchSvg } from "../../assets/search.svg";
 import { ReactComponent as SettingsSvg } from "../../assets/settings.svg";
+import { ReactComponent as BigSearchSvg } from "../../assets/big_search.svg";
 
 interface AppProps {
   initialState: Array<StockItem>,
@@ -19,6 +20,7 @@ interface State {
   from: number,
   to: number,
   displaySearchForm: boolean,
+  displayFilterForm: boolean,
 }
 
 class App extends React.Component<AppProps, State> {
@@ -29,6 +31,7 @@ class App extends React.Component<AppProps, State> {
     from: 0,
     to: 0,
     displaySearchForm: false,
+    displayFilterForm: false,
   };
 
   async componentDidMount() {
@@ -40,6 +43,7 @@ class App extends React.Component<AppProps, State> {
       stocks: data,
       name: "",
       displaySearchForm: false,
+      displayFilterForm: false,
     })
   }
 
@@ -67,10 +71,17 @@ class App extends React.Component<AppProps, State> {
     });
   };
 
-  handleFilterClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  handleSearchClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     this.setState({
       displaySearchForm: !this.state.displaySearchForm,
+    });
+  };
+
+  handleFilterClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    this.setState({
+      displayFilterForm: !this.state.displayFilterForm,
     });
   };
 
@@ -105,64 +116,76 @@ class App extends React.Component<AppProps, State> {
 
   render() {
     return (
-        <div className="App">
-          <div className="logo-and-options">
-            <p className="logo">STOKR</p>
-            <ul className="options">
-              <li>
-                <button aria-label="Search" className="navigation-button">
-                  <SearchSvg />
-                </button>
-              </li>
-              <li>
-                <button aria-label="Refresh" onClick={this.handleRefreshClick} className="navigation-button">
-                  <RefreshSvg />
-                </button>
-              </li>
-              <li>
-                <button aria-label="Filter" onClick={this.handleFilterClick} className="navigation-button">
-                  <FilterSvg />
-                </button>
-              </li>
-              <li>
-                <button aria-label="Settings" className="navigation-button">
-                  <SettingsSvg />
-                </button>
-              </li>
-            </ul>
-          </div>
-
-          {
-            (this.state.displaySearchForm ?
-            <form action="./apply.html" className="search-form">
-              <div className="inputs">
-                <div className="input-column">
-                  <div className="by-name">
-                    <span className="input-description">By Name</span><input type="text" className="search-input"
-                                                                             onChange={this.handleNameChange}/>
-                  </div>
-                  <div className="by-gain">
-                    <span className="input-description">By Gain</span><input type="text" className="search-input"
-                                                                             onChange={this.handleGainChange}/>
-                  </div>
-                </div>
-                <div className="input-column">
-                  <div className="by-range-from">
-                    <span className="input-description">By Range: From</span><input type="number" min="0"
-                                                                                    className="search-input" onChange={this.handleFromChange}/>
-                  </div>
-                  <div className="by-range-to">
-                    <span className="input-description">By Range: To</span><input type="number" min="0"
-                                                                                  className="search-input" onChange={this.handleToChange}/>
-                  </div>
-                </div>
+        (this.state.displaySearchForm ?
+            <div>
+              <div className="search-by-name-header">
+                <input className="search-by-name-input"/>
+                <button className="search-by-name-button" onClick={this.handleSearchClick}>Cancel</button>
               </div>
-              <button onClick={this.handleSubmitClick} className="apply">Apply</button>
-            </form> : <div></div>)
-          }
+              <div className="search-by-name-image-and-text">
+                <BigSearchSvg />
+                <p>Search</p>
+              </div>
+            </div>
+          :
+            <div className="App">
+              <div className="logo-and-options">
+                <p className="logo">STOKR</p>
+                <ul className="options">
+                  <li>
+                    <button aria-label="Search" onClick={this.handleSearchClick} className="navigation-button">
+                      <SearchSvg />
+                    </button>
+                  </li>
+                  <li>
+                    <button aria-label="Refresh" onClick={this.handleRefreshClick} className="navigation-button">
+                      <RefreshSvg />
+                    </button>
+                  </li>
+                  <li>
+                    <button aria-label="Filter" onClick={this.handleFilterClick} className="navigation-button">
+                      <FilterSvg />
+                    </button>
+                  </li>
+                  <li>
+                    <button aria-label="Settings" className="navigation-button">
+                      <SettingsSvg />
+                    </button>
+                  </li>
+                </ul>
+              </div>
 
-          <Stocks items={this.state.stocks} />
-        </div>
+              {
+                (this.state.displayFilterForm ?
+                <form action="./apply.html" className="search-form">
+                  <div className="inputs">
+                    <div className="input-column">
+                      <div className="by-name">
+                        <span className="input-description">By Name</span><input type="text" className="search-input"
+                                                                                 onChange={this.handleNameChange}/>
+                      </div>
+                      <div className="by-gain">
+                        <span className="input-description">By Gain</span><input type="text" className="search-input"
+                                                                                 onChange={this.handleGainChange}/>
+                      </div>
+                    </div>
+                    <div className="input-column">
+                      <div className="by-range-from">
+                        <span className="input-description">By Range: From</span><input type="number" min="0"
+                                                                                        className="search-input" onChange={this.handleFromChange}/>
+                      </div>
+                      <div className="by-range-to">
+                        <span className="input-description">By Range: To</span><input type="number" min="0"
+                                                                                      className="search-input" onChange={this.handleToChange}/>
+                      </div>
+                    </div>
+                  </div>
+                  <button onClick={this.handleSubmitClick} className="apply">Apply</button>
+                </form> : <div></div>)
+              }
+
+              <Stocks items={this.state.stocks} />
+            </div>)
     );
   }
 }
